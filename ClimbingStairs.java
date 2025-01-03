0895299fd9600b9400ddeed3348ebccf05978857
nstone213 <nbaronstone@gmail.com>
2025-01-02 22:40:18 -0500

    Modified file

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,8 +1,10 @@
-class ClimbingStairs {
-    public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        return climbStairs(n-1) + climbStairs(n-2);
-    }
-}
+// TLE (Time Limit Exceeded) Solution
+//
+// class ClimbingStairs {
+//    public int climbStairs(int n) {
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        return climbStairs(n-1) + climbStairs(n-2);
+//    }
+// }
